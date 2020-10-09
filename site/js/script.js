6acc1075a05f32f65380e8f1ed4cc6d3efd20427
ht1204 <ptb1204@hotmail.com>
2020-10-09 17:50:38 -0500

    changes in html DOM

--- site/js/script.js
+++ site/js/script.js
@@ -1,3 +1,16 @@
-function show() {
-    alert("Hi there");
+const loadFunction = () => {
+    document.getElementsByTagName('p')[0].onclick = show
+    document.getElementById("btnID").onclick = showAlertbtn
+    document.getElementById("tblTest").style.display = "none"
+}
+
+
+const show = () => {
+    alert('Hi there')
+    document.getElementById("tblTest").style.display = "none"
+}
+
+const showAlertbtn = () => {
+    alert("Hi, I'm a button")
+    document.getElementById("tblTest").style.display = "inline-block"
 }
